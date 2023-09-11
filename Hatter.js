068006661491f73983d3a53335753835a0461ba5
Domi_Nate <90252368+DomiNate2@users.noreply.github.com>
2023-09-11 19:42:41 +0200

    Add files via upload

---
+++ Hatter.js
@@ -0,0 +1,15 @@
+
+function first()
+{
+    document.body.style.backgroundImage="url('bg1.jpg')";
+}
+
+function second()
+{
+    document.body.style.backgroundImage="url('bg2.jpg')";
+}
+
+function third()
+{
+    document.body.style.backgroundImage="url('bg3.jpg')";
+}
